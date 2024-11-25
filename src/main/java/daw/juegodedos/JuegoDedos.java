162af96ceeaae02642bed7158cf253f6a4f69128
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-11-25 12:28:04 +0100

    start project

---
+++ src/main/java/daw/juegodedos/JuegoDedos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw.juegodedos;
+
+/**
+ *
+ * @author alvaro
+ */
+public class JuegoDedos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
