7ffb30d4651b9b8b52b8b9dc9410de0898810df4
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-11-26 09:21:38 +0100

    fix none guess the result

--- src/main/java/daw/juegodedos/JuegoDedos.java
+++ src/main/java/daw/juegodedos/JuegoDedos.java
@@ -16,18 +16,59 @@ public class JuegoDedos {
     public static void main(String[] args) {
         String text1 = "Introduce cuantos dedos vas a sacar: ";
         String text2 = "Introduce el total de dedos: ";
-        
+
         int fingUser = 0, totalUser = 0;
         int fingMach = 0, totalMach = 0;
+        int res = 0;
         int winUser = 0, winMach = 0;
-        
+
         fingUser = getNumberFromUser(0, text1);
         totalUser = getNumberFromUser(fingUser, text2);
-        
+
         fingMach = getNumberFromMachine();
         totalMach = getNumberFromMachine(fingMach);
+
+        res = getResult(fingUser, totalUser, fingMach, totalMach);
+        
+        switch (res) {
+            case 0 -> {
+                showSelected(fingUser, totalUser, fingMach, totalMach);
+                System.out.println("El resultado es EMPATE.");
+            }
+            case 1 -> {
+                showSelected(fingUser, totalUser, fingMach, totalMach);
+                System.out.println("El ganador es el USUARIO.");
+
+                winUser++;
+                winMach = 0;
+            }
+            case 2 -> {
+                showSelected(fingUser, totalUser, fingMach, totalMach);
+                System.out.println("El ganador es la MAQUINA.");
+
+                winMach++;
+                winUser = 0;
+            }
+            case 3 -> {
+                showSelected(fingUser, totalUser, fingMach, totalMach);
+                System.out.println("Ninguno ha acertado.");
+            }
+            default ->
+                System.out.println("Error en el resultado.");
+        }
     }
-    
+
+    public static void showSelected(int nUser, int tUser, int nMach,
+            int tMach) {
+        String text = """
+                      Dedos Usuario: %d
+                      Total Usuario: %d
+                      Dedos Maquina: %d
+                      Total Maquina: %d
+                      """.formatted(nUser, tUser, nMach, tMach);
+        System.out.println(text);
+    }
+
     public static int getNumberFromUser(int min, String text) {
         Scanner sc = new Scanner(System.in);
         int max = min + 6;
@@ -48,27 +89,30 @@ public class JuegoDedos {
             }
         } while (true);
     }
-    
+
     public static int getNumberFromMachine() {
         Random rd = new Random();
-        
+
         return rd.nextInt(0, 6);
     }
-    
+
     public static int getNumberFromMachine(int min) {
         Random rd = new Random();
-        
+
         return rd.nextInt((min + 1), (min + 6));
     }
-    
-    public static int getResult(int numUser, int totalUser, int numMach, 
+
+    public static int getResult(int numUser, int totalUser, int numMach,
             int totalMach) {
         // Si es 0 lo convertimos en 1, porque 0 dedos vale 1
         numUser = numUser == 0 ? 1 : numUser;
         numMach = numMach == 0 ? 1 : numMach;
-        
+
+        System.out.println(numUser);
+        System.out.println(numMach);
+
         int totalNum = numUser + numMach;
-        
+        System.out.println(totalNum);
         if (totalNum == totalUser && totalNum == totalMach) {
             return 0;
         } else if (totalNum == totalUser) {
@@ -76,7 +120,7 @@ public class JuegoDedos {
         } else if (totalNum == totalMach) {
             return 2;
         } else {
-            return -1;
+            return 3;
         }
     }
 }
