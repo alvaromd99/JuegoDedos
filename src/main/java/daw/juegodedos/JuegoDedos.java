6027bf6ede2c64bcfec5c7bfa0e3ad7b8816758b
alvaromd99 <117214600+alvaromd99@users.noreply.github.com>
2024-11-26 08:34:55 +0100

    make method to ask user for a number

--- src/main/java/daw/juegodedos/JuegoDedos.java
+++ src/main/java/daw/juegodedos/JuegoDedos.java
@@ -1,8 +1,10 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package daw.juegodedos;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  *
@@ -12,5 +14,25 @@ public class JuegoDedos {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    public static int getNumberFromUser(int min, int max, String text) {
+        Scanner sc = new Scanner(System.in);
+        int num = 0;
+
+        do {
+            System.out.println(text);
+            try {
+                num = sc.nextInt();
+                if (num < min || num > max) {
+                    System.out.println("Error, el numero no tiene sentido.");
+                } else {
+                    return num;
+                }
+            } catch (InputMismatchException ime) {
+                System.out.println("Error, no has introducido un numero.");
+                sc.nextLine();
+            }
+        } while (true);
     }
 }
